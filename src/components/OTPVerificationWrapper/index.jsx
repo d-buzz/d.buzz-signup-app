b02dc619225449015c00377b4a9a27080e4bcb02
Gerard Baluyot <gacbaluyot@gmail.com>
2023-10-16 08:04:08 +0800

    Fixed OTP, added Countdown on Expiry

--- src/components/OTPVerificationWrapper/index.jsx
+++ src/components/OTPVerificationWrapper/index.jsx
@@ -35,14 +35,42 @@ const OTPVerificationWrapper = (props) => {
 
 	const [requestedCodeAgainIn, setRequestedCodeAgainIn] = useState(0)
 
+
+	const [otpExpiryTimeLeft, setOtpExpiryTimeLeft] = useState(null);
+
+	const [otpError, setOtpError] = useState("");
+
+	// added limit for set otp
+	useEffect(() => {
+		if (codeRequested) {
+			setOtpExpiryTimeLeft(15);  // Set to 15 minutes in seconds
+			const otpExpiryInterval = setInterval(() => {
+				setOtpExpiryTimeLeft(prevTime => prevTime - 1);
+			}, 1000);
+
+			return () => clearInterval(otpExpiryInterval);
+		}
+	}, [codeRequested]);
+
+	const [showResendLink, setShowResendLink] = useState(false);
+
+	useEffect(() => {
+		if (otpExpiryTimeLeft === 0 && otpExpiryTimeLeft !== null && codeRequested) {
+			setOtpError("The OTP has expired. Please request a new one.");
+			setShowResendLink(true); // Show the Resend OTP link once the OTP has expired
+		}
+	}, [otpExpiryTimeLeft, codeRequested]);
+
+
 	useEffect(() => {
 		const requestCodeLimitDateTime = localStorage.getItem('requestCodeLimitDateTime')
 		const elapsedTime = moment().diff(requestCodeLimitDateTime, 'minutes')
 
-		if(elapsedTime > 30) {
+		if(elapsedTime > 15) {
 			localStorage.clear()
 		}
 	}, [])
+
 
 	useEffect(() => {
 		var timeLeft = 15
@@ -94,46 +122,64 @@ const OTPVerificationWrapper = (props) => {
 	return (
 		<div className="pt-[100px] h-full flex flex-col justify-center items-center">
 			<div className="flex flex-col items-start">
-				<div className="flex items-center justify-center select-none cursor-pointer hover:opacity-50 transition-all" onClick={() => setShowPhoneVerifier(false)}>
-					<img src={BackIcon} alt='back icon' className="h-[30px] pb-[2px] pr-[2px]" />
+				<div
+					className="flex items-center justify-center select-none cursor-pointer hover:opacity-50 transition-all"
+					onClick={() => setShowPhoneVerifier(false)}>
+					<img src={BackIcon} alt='back icon' className="h-[30px] pb-[2px] pr-[2px]"/>
 					<span className="pl-2 text-[#e61c34] font-bold">Go back</span>
 				</div>
 				<div className="flex flex-col items-center justify-center">
 					<span className="mt-[20px] text-[22px] md:text-[26px] lg:text-[32px] font-bold">One Time Passcode Verification</span>
-					{
-						!codeRequested && !accountCreated
-						?
-							<div>
-								<InputField placeholder='Enter your phone number' className='w-[100%] md:w-[400px] lg:w-[400px] mt-4 mb-2 text-[20px] md:text-[30px] lg:text-[30px]' type='phone' value={phone} onChange={setPhone} disabled={submitting || codeRequested} />
-								{phoneNumberError && (
-									<span className={`flex justify-center pr-4 mt-[5px] mb-4 text-[14px] font-medium`} style={{ color: '#dc3545' }}>
-										{phoneNumberError}
-									</span>
-								)}
-							</div>
-							:
+					{!codeRequested && !accountCreated
+						? <div>
+							<InputField placeholder='Enter your phone number'
+										className='w-[100%] md:w-[400px] lg:w-[400px] mt-4 mb-2 text-[20px] md:text-[30px] lg:text-[30px]'
+										type='phone' value={phone} onChange={setPhone}
+										disabled={submitting || codeRequested}/>
+							{phoneNumberError && (
+								<span className={`flex justify-center pr-4 mt-[5px] mb-4 text-[14px] font-medium`}
+									  style={{color: '#dc3545'}}>
+                                    {phoneNumberError}
+                                </span>
+							)}
+						</div>
+						: <div>
 							<OTPCodeInput handleVerifyOpt={handleVerifyOpt} setConfirmationCode={setConfirmationCode}/>
+							{otpError && (
+								<div className={`flex justify-center pr-4 mt-[5px] mb-4 text-[14px] font-medium`}>
+									<span style={{color: '#dc3545'}}>
+										{otpError}
+									</span>
+									{showResendLink && (
+										<button
+											onClick={(event) => handleRequestOTPCode(event)}
+											className="ml-2 text-[#007bff] cursor-pointer hover:underline"
+										>
+											Resend OTP
+										</button>
+									)}
+								</div>
+							)}
+						</div>
 					}
-					{
-						!codeRequested && !accountCreated
-						?
-							<div className='relative mt-4 flex flex-col items-center justify-center'>
-								<Button variant='fill' onClick={handleRequestOTPCode} disabled={requestedCodeLimit} loading={submitting || checkingPhoneNumber}>SEND CODE</Button>
-								{
-									requestedCodeCount > 0 && requestedCodeAgainIn > 0 &&
-									<span className='mt-4 md:mt-0 lg:mt-0 md:absolute lg:md:absolute right-[-120px] text-[12px] text-gray-400'>Request another <br /> code in {requestedCodeAgainIn}</span>
-								}
-								{
-									requestedCodeLimit && requestedCodeAgainIn === 0 &&
-									<span className='mt-4 font-medium text-center'>You've already requested code more than 3 times, <br /> please check back later in 30 minutes.</span>
-								}
-							</div>
-						:
-							<Button className='mt-4' variant='fill' onClick={handleVerifyOpt} disabled={submitting} loading={submitting}>VERIFY CODE</Button>
+					{!codeRequested && !accountCreated
+						? <div className='relative mt-4 flex flex-col items-center justify-center'>
+							<Button variant='fill' onClick={(event) => handleRequestOTPCode(event)}
+									disabled={requestedCodeLimit} loading={submitting || checkingPhoneNumber}>SEND
+								CODE</Button>
+							{requestedCodeCount > 0 && requestedCodeAgainIn > 0 &&
+								<span
+									className='mt-4 md:mt-0 lg:mt-0 md:absolute lg:md:absolute right-[-120px] text-[12px] text-gray-400'>Request another <br/> code in {requestedCodeAgainIn}</span>
+							}
+							{requestedCodeLimit && requestedCodeAgainIn === 0 &&
+								<span className='mt-4 font-medium text-center'>You've already requested code more than 3 times, <br/> please check back later in 30 minutes.</span>
+							}
+						</div>
+						: <Button className='mt-4' variant='fill' onClick={handleVerifyOpt} disabled={submitting}
+								  loading={submitting}>VERIFY CODE</Button>
 					}
-					{
-						accountCreated &&
-						<PageLoading />
+					{accountCreated &&
+						<PageLoading/>
 					}
 				</div>
 			</div>
